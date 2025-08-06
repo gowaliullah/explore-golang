32c3a21f6e55dc03aa3132fb9bb5ce775bc263e8
MD Waliullah <waliullah.godev@gmail.com>
2025-08-06 06:09:16 +0000

    slice from array and existing slice

--- slice/main.go
+++ slice/main.go
@@ -1,13 +1,20 @@
 package main
 
-import "fmt"
-
 func main() {
-	arr := [6]string{"This", "is", "a", "GO", "interview", "question"}
 
-	s := arr[1:4]
+	/*
 
-	fmt.Println(s)
-	fmt.Println("len: ", len(arr))
-	fmt.Println("Cap: ", cap(arr))
+		// first step of learing slice
+
+		arr := [6]string{"This", "is", "a", "GO", "interview", "question"} // tmi
+
+		s := arr[1:4] // "is", "a", "GO"  // haat
+
+		s1 := s[1:2] // "a" // ring
+
+		fmt.Println(s1)
+		fmt.Println("len: ", len(s1))
+		fmt.Println("Cap: ", cap(s1))
+
+	*/
 }
