a16e4858fc2abc396c915c235e682c2550afd1a0
MD Waliullah <waliullah.godev@gmail.com>
2025-08-09 06:58:54 +0000

    formet with %

--- data_type/main.go
+++ data_type/main.go
@@ -6,12 +6,20 @@ func main() {
 
 	var p int8 = -128
 	var q uint8 = 255 // unsigned ( 0 and only positive number)
-	var z float32 = 20.40
+	var z float32 = 20.40323
 	var ha bool = false // 8 bits - 1 byte
+
+	haa := '💖'
+
+	var hab rune = '💖'
+
+	fmt.Println(p, q, z, ha, haa)
+	fmt.Printf("%d\n", p)
+	fmt.Printf("%f\n", z)
+	fmt.Printf("%c\n", hab)
 
 	// byte -> alias for unit8 -> 8 bits -> 1 byte
 
-	// rune -> alias for init32 (unicode point) -> 32 bits -> 4 bytes %c
+	// rune -> alias for init32 (unicode point) -> 32 bits -> 4 bytes -> rune ar format holo: %c (without english any charactar is rune/রুন)
 
-	fmt.Println(p, q, z, ha)
 }
