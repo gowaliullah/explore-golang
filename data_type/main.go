b747af887b00d2bbbb8c4b66d8df986d05270dec
MD Waliullah <waliullah.godev@gmail.com>
2025-08-09 06:42:31 +0000

    bool for only true/false value stored with 8 bits memory allocated

--- data_type/main.go
+++ data_type/main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
+
 	var p int8 = -128
-
 	var q uint8 = 255 // unsigned ( 0 and only positive number)
-
 	var z float32 = 20.40
+	var ha bool = false // 8 bits - 1 byte
 
-	fmt.Println(p, q, z)
+	fmt.Println(p, q, z, ha)
 }
