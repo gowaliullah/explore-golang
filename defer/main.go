ca9a592694bd02fc2a8a74dd4ecdd9624287ab4d
MD Waliullah <waliullah.godev@gmail.com>
2025-08-12 11:49:39 +0000

    write all defer rule

--- defer/main.go
+++ defer/main.go
@@ -1,21 +1,74 @@
+// defer fucntion ---> under the hood closer
+
+// parent & closer jodi exists kore tahole closer variable ar address refer share kore
+
+// defer list pointer
+
+// defer --> linked list data stracture use kore
+
+/*
+
+named returned values rules
+
+1. all code executed
+2. defer function store kora hobe magic box e
+3. return --> all defer fucntion execute korbe
+4. return korbe named variables gular values
+
+
+just return types
+1. all code executed
+2. defer function store kora hobe magic box e
+3. return values are evaluated at this time (store the return value) // ( static )
+4. all defer functions execute korbe
+
+
+*/
+
 package main
 
 import "fmt"
 
-func a() {
+func calculate() (result int) {
+	fmt.Println("first", result)
 
-	i := 0
+	sum := func() {
+		result = result + 10
+		fmt.Println("defer", result)
+	}
 
-	fmt.Println("First: ", i)
+	defer sum()
 
-	defer fmt.Println("Second: ", i)
+	result = 5
+	fmt.Println("second", result)
 
-	i++
+	return
 
-	fmt.Println("Third: ", i)
-	defer fmt.Println("Forth: ", i)
+}
+
+func cal() int {
+
+	result := 0
+
+	fmt.Println("first", result)
+
+	sum := func() {
+		result = result + 10
+		fmt.Println("defer", result)
+	}
+
+	defer sum()
+
+	result = 5
+	fmt.Println("second", result)
+
+	return result
+
 }
 
 func main() {
-	a()
+	// a := calculate()
+	// fmt.Println(a)
+	b := cal()
+	fmt.Println(b)
 }
--- named-return-values/main.go
+++ named-return-values/main.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
-func sum(a, b int) (result int) {
+func sum(a, b int) (result int) { // named return value
 	result = a + b
 
 	return //  default return result
+}
+
+func sum2(a, b int) int {
+	result := a + b
+	return result
 }
 
 func main() {
