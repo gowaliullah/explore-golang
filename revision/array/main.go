36a4b9b483d212f742ed299e556c7960ed1f77a1
MD Waliullah <waliullah.godev@gmail.com>
2025-08-25 12:16:44 +0600

    for loop using range

--- revision/array/main.go
+++ revision/array/main.go
@@ -5,7 +5,14 @@ import "fmt"
 func main() {
 	nums := [5]int{1, 2, 3, 4, 5}
 
+	fmt.Println("For Loop")
 	for i := 0; i < len(nums); i++ {
-		fmt.Println(nums[i])
+		fmt.Println(i, nums[i])
 	}
+
+	fmt.Println("For Loop with range")
+	for index, value := range nums {
+		fmt.Println(index, value)
+	}
+
 }
