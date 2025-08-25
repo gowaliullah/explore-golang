be37242459b5f53e0c41eafb494234585e31065d
MD Waliullah <waliullah.godev@gmail.com>
2025-08-25 12:26:11 +0600

    arrayOfArrays

--- revision/array/main.go
+++ revision/array/main.go
@@ -29,6 +29,16 @@ func multidimensionalArray() {
 	fmt.Println(matrix)
 }
 
+func arrayOfArrays() {
+	arr := [][]int{
+		{1, 2},
+		{3, 4},
+		{5},
+		{6, 7, 8},
+	}
+	fmt.Println(arr)
+}
+
 func main() {
 	arrayOfArrays()
 }
