251fc5e6d51529bc4080aeb747e88e6e77c6d15c
MD Waliullah <waliullah.godev@gmail.com>
2025-09-17 07:04:27 +0600

    implement interface

--- interface/main.go
+++ interface/main.go
@@ -8,10 +8,14 @@ type user struct {
 	Money float64
 }
 
-type User interface {
+// only signatures of the functions
+type People interface { // pure abstructs
+	PrintDetails()
+	ReceivedMoney(amount float64) float64
 }
 
-func PrintDetails(obj user) {
+// receiver func or methods or behaviour
+func (obj user) PrintDetails() {
 	fmt.Println("Name: ", obj.Name)
 	fmt.Println("Age: ", obj.Age)
 	fmt.Println("Money: ", obj.Money)
@@ -23,5 +27,5 @@ func main() {
 		Age:   30,
 		Money: 344.423,
 	}
-	PrintDetails(usr1)
+	usr1.PrintDetails()
 }
