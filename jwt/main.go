fb9218efc2f01aa6b493f5ec3cc571b56d54268a
MD Waliullah <waliullah.godev@gmail.com>
2025-10-11 21:09:26 +0600

    crypto/sha256

--- jwt/main.go
+++ jwt/main.go
@@ -1,25 +1,36 @@
 package main
 
 import (
-	"encoding/base64"
+	"crypto/sha256"
 	"fmt"
 )
 
 func main() {
-	s := "098fg"
 
-	byteArr := []byte(s)
+	data := []byte("Hello")
 
-	enc := base64.URLEncoding.WithPadding(base64.NoPadding)
-	b64str := enc.EncodeToString(byteArr)
+	hash := sha256.Sum256(data)
 
-	decodedStr, err := enc.DecodeString(b64str)
+	fmt.Println(hash)
 
-	if err != nil {
-		fmt.Println(err)
-	}
+	/*
+		s := "098fg"
 
-	fmt.Println(b64str)
-	fmt.Println(decodedStr)
+		byteArr := []byte(s)
+
+		enc := base64.URLEncoding.WithPadding(base64.NoPadding)
+		b64str := enc.EncodeToString(byteArr)
+
+		decodedStr, err := enc.DecodeString(b64str)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(b64str)
+		fmt.Println(decodedStr)
+
+	*/
 
 }
