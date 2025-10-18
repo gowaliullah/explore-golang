ff74e824117c170998ce704a3005a581d4d0eb2b
MD Waliullah <waliullah.godev@gmail.com>
2025-10-18 09:49:10 +0600

    more explore

--- recap-interface/main.go
+++ recap-interface/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 type user struct {
 	Name  string
 	Age   int
@@ -12,6 +17,11 @@ type People interface {
 
 func (obj user) added(m float64) float64 {
 	obj.Money = obj.Money + m
+	return obj.Money
+}
+
+func (obj user) withdrawMoney(m float64) float64 {
+	obj.Money = obj.Money - m
 	return obj.Money
 }
 
@@ -29,5 +39,25 @@ func main() {
 	}
 
 	usr1.added(1000000)
+
+	var usr2 Owner
+	usr2 = user{
+		Name:  "Habi",
+		Age:   19,
+		Money: 1234953.434,
+	}
+
+	usr2.withdrawMoney(8998878)
+
+	obj, ok := usr2.(user)
+
+	if !ok {
+		fmt.Println("sorry..! usr2 is not type of user struct")
+		os.Exit(1)
+	}
+
+	obj.added(12213232423445.5345)
+
+	obj.withdrawMoney(24343.4324)
 
 }
